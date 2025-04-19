f35bd8cab95a7fce9ef5965e7643196ad89bfde9
Hamza Anver <hamzaanver1234@gmail.com>
2025-04-19 16:04:46 +0400

    improved layout added baudrate selection

--- app/terminal/terminaltab.py
+++ app/terminal/terminaltab.py
@@ -9,9 +9,23 @@ class Terminal:
     def __init__(self, ui: ui) -> None:
         self.ui = ui
         self.port_select = None
-        self.baudrate = None
+        self.port_select_options: List[str] = []
+
+        self.baudrate_select = None
+        self.baudrate_select_options: List[int] = [
+            300,
+            1200,
+            2400,
+            4800,
+            9600,
+            14400,
+            19200,
+            38400,
+            57600,
+            115200
+        ]
         self.serial = None
-        self.available_ports: List[str] = []
+        
 
         ui.timer(1.0, self.auto_update)
 
@@ -19,16 +33,16 @@ class Terminal:
         if self.port_select:
             # If there is a change in available ports, update the select options
             current_ports = self.get_ports()
-            if set(current_ports) != set(self.available_ports):
+            if set(current_ports) != set(self.port_select_options):
                 # Update the select options
                 self.port_select.options = current_ports
                 self.port_select.update()
-                if self.port_select.value not in self.available_ports:
+                if self.port_select.value not in self.port_select_options:
                     self.port_select.value = None
 
                 # Figure out if ports have been added or removed
-                added_ports = set(current_ports) - set(self.available_ports)
-                removed_ports = set(self.available_ports) - set(current_ports)
+                added_ports = set(current_ports) - set(self.port_select_options)
+                removed_ports = set(self.port_select_options) - set(current_ports)
 
                 # Notify the user about added or removed ports
                 if added_ports:
@@ -46,15 +60,17 @@ class Terminal:
                     )
 
                 # Update the available ports
-                self.available_ports = current_ports
+                self.port_select_options = current_ports
 
     def get_ports(self) -> List[str]:
         ports = serial.tools.list_ports.comports()
         list_ports = [port.device for port in ports]
-        # print(f"Available ports: {self.available_ports}")
+        # print(f"Available ports: {self.port_select_options}")
         return list_ports
 
     def term_tab(self) -> None:
         with self.ui.row():
-            self.available_ports = self.get_ports()
-            self.port_select = ui.select(label="Port", options=self.available_ports).classes("w-64") 
+            self.port_select = ui.select(label="Port", options=[]).classes("w-64")
+            self.baudrate_select = ui.select(label="Baudrate", options=self.baudrate_select_options, value=115200).classes("w-64")
+                
+            
--- main.py
+++ main.py
@@ -11,29 +11,27 @@ def dash_tab():
     with ui.card().style('background-color: #f0f0f0').classes('w-full h-full'):
         ui.label('Dashboard content goes here.')
 
-with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
-    ui.label('OmniStim Controller').classes('text-2xl text-white')
+with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between p-0'):
+    ui.label('OmniStim Controller').classes('text-2xl text-white m-3')
+    with ui.tabs().classes('m-0 p-0') as tabs:
+        dashboard = ui.tab('Dashboard', icon='multiline_chart')
+        calculator = ui.tab('Calculator', icon='calculate')
+        terminal = ui.tab('Terminal', icon='terminal')
+        info = ui.tab('Info', icon='info')
+
+
+with ui.tab_panels(tabs, value=dashboard).classes('w-full h-full'):
+    with ui.tab_panel(dashboard):
+        ui.label('Dashboard content goes here.')
+    with ui.tab_panel(calculator):
+        ui.label('Calculator content goes here.')
+    with ui.tab_panel(terminal):
+        term_tab.term_tab()
+    with ui.tab_panel(info):
+        ui.label('Info content goes here.')
+
 
 with ui.footer(elevated=True).style('background-color: #737475').classes('items-center justify-between text-align-center'):
     ui.label('For the OmniStim Eval Board, made by Hamza Anver').classes('text-sm text-white')
-
-with ui.splitter(value=30).classes('w-full max-w-lg h-full') as splitter:
-    with splitter.before:
-        with ui.tabs().props('vertical').classes('w-full') as tabs:
-            dashboard = ui.tab('Dashboard', icon='multiline_chart')
-            calculator = ui.tab('Calculator', icon='calculate')
-            terminal = ui.tab('Terminal', icon='terminal')
-            info = ui.tab('Info', icon='info')
-    with splitter.after:
-        with ui.tab_panels(tabs, value=dashboard) \
-                .props('vertical').classes('w-full h-full'):
-            with ui.tab_panel(dashboard):
-                dash_tab()
-            with ui.tab_panel(calculator):
-                ui.label('Calculator content goes here.')
-            with ui.tab_panel(terminal):
-                term_tab.term_tab()
-            with ui.tab_panel(info):
-                ui.label('Info content goes here.')
 
 ui.run()
